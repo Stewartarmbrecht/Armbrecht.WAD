7d6c682840cbd01f6a0fe1cdacd7e44121b4817e
Stewart Armbrecht <stewartarmbrecht@gmail.com>
2012-08-10 19:56:46 -0500

    Initial Check-In

---
+++ js/indexVM.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: stewartarmbrecht
+ * Date: 8/10/12
+ * Time: 10:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var indexVM = function() {
+
+    var headerText = "This is a test of the VM.";
+
+    return {
+        headerText: headerText
+    }
+}();
+
+ko.applyBindings(indexVM);
